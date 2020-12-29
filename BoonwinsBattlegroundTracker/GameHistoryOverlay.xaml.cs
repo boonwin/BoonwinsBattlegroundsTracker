c7a4b6323ef329819f8138dd58ec348914dc03d9
Björn Kreye <b.kreye@outlook.com>
2020-12-29 16:50:02 +0100

    i added best hero and changed for now the viewlist to datagrid

--- BoonwinsBattlegroundTracker/GameHistoryOverlay.xaml.cs
+++ BoonwinsBattlegroundTracker/GameHistoryOverlay.xaml.cs
@@ -36,65 +36,16 @@ namespace BoonwinsBattlegroundTracker
             imgBg.ImageSource = new BitmapImage(new Uri(Config._statsBackgroundPath));
             imgStatsFG.Source = new BitmapImage(new Uri(Config._statsBestHeroForegroundPath));
             imgStatsBG.Source = new BitmapImage(new Uri(Config._statsBestHeroBackgroundPath));
+            mostTop3Hero();
 
         }
 
-        GridViewColumnHeader _lastHeaderClicked = null;
-        ListSortDirection _lastDirection = ListSortDirection.Ascending;
-
-        void GridViewColumnHeaderClickedHandler(object sender, RoutedEventArgs e)
-        {
-            GridViewColumnHeader headerClicked = e.OriginalSource as GridViewColumnHeader;
-            ListSortDirection direction;
-
-            if (headerClicked != null)
-            {
-                if (headerClicked.Role != GridViewColumnHeaderRole.Padding)
-                {
-                    if (headerClicked != _lastHeaderClicked)
-                    {
-                        direction = ListSortDirection.Ascending;
-                    }
-                    else
-                    {
-                        if (_lastDirection == ListSortDirection.Ascending)
-                        {
-                            direction = ListSortDirection.Descending;
-                        }
-                        else
-                        {
-                            direction = ListSortDirection.Ascending;
-                        }
-                    }
-
-                    string header = headerClicked.Column.Header as string;
-                    Sort(header, direction);
-
-                    _lastHeaderClicked = headerClicked;
-                    _lastDirection = direction;
-                }
-            }
-        }
-
-        private void Sort(string sortBy, ListSortDirection direction)
-        {
-            ICollectionView dataView =
-              CollectionViewSource.GetDefaultView(lbtHeros.ItemsSource);
-
-            dataView.SortDescriptions.Clear();
-            SortDescription sd = new SortDescription(sortBy, direction);
-            dataView.SortDescriptions.Add(sd);
-            dataView.Refresh();
-        }
+        
 
             public void showAvgRanks()
         {
             int ranks = 0;
             int counter = 0;
-            int bestHero = 0;
-            string heroName;
-
-
 
             foreach (var game in _recordList)
             {
@@ -112,18 +63,104 @@ namespace BoonwinsBattlegroundTracker
         {
             var viewList = _recordList.GroupBy(t => new { Hero = t.Hero })
              .Select(g => new {
-                 HeroImage = g.Select(l => l.HeroID), 
-                 Average = Convert.ToInt32(Math.Round(g.Average(p => p.Position))),
+                 //HeroImage = g.Select(l => l.HeroID),
+                 Hero = g.Key.Hero,
                  Count = g.Count(),
-                 Hero = g.Key.Hero
+                 Average = Math.Round(g.Average(p => p.Position),1)
+                 
+                 
              })
              .OrderByDescending(o => o.Count)
              ;
 
-            lbtHeros.ItemsSource = viewList;
+            dgGameHistory.ItemsSource = viewList;
         }
 
+        public void mostTop3Hero()
+        {
+
+            var topThreePositionPerHero = _recordList
+                .Where(x => x.Position <= 3)
+                .GroupBy(t => new {Hero = t.Hero})
+                .Select(g => new {
+                    Hero = g.Key.Hero,
+                    Count = g.Count()
+                })
+                .ToList();
+
+
+            var bottomFivePositionPerHero = _recordList
+                .Where(x => x.Position > 3)
+                .GroupBy(t => new { Hero = t.Hero })
+                .Select(g => new {
+                    Hero = g.Key.Hero,
+                    Count = g.Count()
+                })
+                .ToList();
+
+
+
+            var query =
+                from heroTop in topThreePositionPerHero
+                join heroBottom in bottomFivePositionPerHero on heroTop.Hero equals heroBottom.Hero
+                select new { Hero = heroTop.Hero, Amount = heroTop.Count - heroBottom.Count}
+                ;
+
+
+            lbBestHero.Content = "Best Hero: " + query.Where(x => x.Amount == query.Max(y => y.Amount)).Select(g => g.Hero).First();
+
+
+        }
 
     }
 
 }
+
+
+//GridViewColumnHeader _lastHeaderClicked = null;
+//ListSortDirection _lastDirection = ListSortDirection.Ascending;
+
+//void GridViewColumnHeaderClickedHandler(object sender, RoutedEventArgs e)
+//{
+//    GridViewColumnHeader headerClicked = e.OriginalSource as GridViewColumnHeader;
+//    ListSortDirection direction;
+
+//    if (headerClicked != null)
+//    {
+//        if (headerClicked.Role != GridViewColumnHeaderRole.Padding)
+//        {
+//            if (headerClicked != _lastHeaderClicked)
+//            {
+//                direction = ListSortDirection.Ascending;
+//            }
+//            else
+//            {
+//                if (_lastDirection == ListSortDirection.Ascending)
+//                {
+//                    direction = ListSortDirection.Descending;
+//                }
+//                else
+//                {
+//                    direction = ListSortDirection.Ascending;
+//                }
+//            }
+
+//            string header = headerClicked.Column.Header as string;
+//            Sort(header, direction);
+
+//            _lastHeaderClicked = headerClicked;
+//            _lastDirection = direction;
+//        }
+//    }
+//}
+
+//private void Sort(string sortBy, ListSortDirection direction)
+//{
+//    ICollectionView dataView =
+//      CollectionViewSource.GetDefaultView(lbtHeros.ItemsSource);
+
+//    dataView.SortDescriptions.Clear();
+//    SortDescription sd = new SortDescription(sortBy, direction);
+//    dataView.SortDescriptions.Add(sd);
+//    dataView.Refresh();
+//}
